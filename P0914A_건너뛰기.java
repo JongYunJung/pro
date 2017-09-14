b2e4324e0bec47c4ff46fb67ac6cde34eada2465
pyjune <pyjune@naver.com>
2017-09-14 15:40:20 +0900

    Create P0914A_건너뛰기.java

---
+++ P0914A_건너뛰기.java
@@ -0,0 +1,47 @@
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class Solution {
+
+	static int [][] A;
+	static int [][]	D;
+	static int N, M;
+	public static void main(String[] args)throws Exception {
+		// TODO Auto-generated method stub
+		System.setIn(new FileInputStream("Text.txt"));
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int tc = 1; tc<=T; tc++)
+		{
+			N = sc.nextInt();
+			M = sc.nextInt();
+			A = new int[N+1][M+1];
+			D = new int[N+1][M+1];
+			for(int i = 1; i<=N; i++)
+				for(int j = 1; j<=M; j++)
+					A[i][j] = sc.nextInt();
+			find();
+			System.out.println("#"+tc+" "+D[N][M]);
+		}
+	}
+	public static void find()
+	{
+		D[1][1] = 1;
+		for(int i = 1; i<=N; i++)
+		{
+			for(int j = 1; j<=M; j++)
+			{
+				for(int k=1; k<j;k++)
+				{
+					if(A[i][k]==j-k)
+						D[i][j] += D[i][k];
+				}
+				for(int k=1; k<i;k++)
+				{
+					if(A[k][j]==i-k)
+						D[i][j] += D[k][j];
+				}
+			}
+		}
+	}
+}
