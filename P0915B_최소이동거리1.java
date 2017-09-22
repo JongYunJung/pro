82c79208b0e876183c50303ae0b77687f66c1593
pyjune <pyjune@naver.com>
2017-09-22 13:01:47 +0900

    Update P0915B_최소이동거리1.java

--- P0915B_최소이동거리1.java
+++ P0915B_최소이동거리1.java
@@ -30,18 +30,23 @@ public class Solution {
 					
 			}
 
-			// 경유지 없는 경우
+			// 경유지 없는 경우. (0은 경유지 집합이 공집합인 경우)
 			for(int i=2; i<=N; i++)
 			{
 				d[i][0] = t[i][1];
 			}
+			// 1을 제외하고 모든 경유지 선택
+			// N=5인경우
+			// 1<<6 : 1000000
+			// -1 :   0111111
+			// &(~3) :0111100 -> {2,3,4,5}
 			
 			int ret = find(1, (((1<<(N+1))-1)&(~3)));
 			
 			System.out.println("#"+tc+" "+ret);
 		}
 	}
-
+	// n은 출발지, s는 경유지 집합의 원소를 표시하는 이진수
 	public static int find(int n, int s)
 	{
 		if(s==0)
@@ -58,7 +63,8 @@ public class Solution {
 			{
 				if((s&(1<<i)) != 0)
 				{
-					ret = t[n][i] + find(i, s&(~(1<<i)));
+					// s&(~(1<<i) : s에서 i를 제외
+					ret = t[n][i] + find(i, s&(~(1<<i))); 
 					if(min>ret)
 						min = ret;
 				}
